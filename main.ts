934f2f48d5b4b2cd58dd28792b9e94359b3df837
Drew Noakes <git@drewnoakes.com>
2014-03-03 11:27:06 +0000

    Updated example to use new version of API.

--- main.ts
+++ main.ts
@@ -13,24 +13,33 @@ enum Side
     Sell
 }
 
+interface IInstrument
+{
+    ric: string;
+    iso2: string;
+}
+
 interface ITrade
 {
     id: number;
-    instrument: string;
+    instrument: IInstrument;
     iso2: string;
     side: Side;
+    status: string;
     quantity: number;
-    filledQuantity: number;
+    filled: number;
 }
 
 var instruments = [
-    {name: 'VOD LN', iso2: 'GB'},
-    {name: 'IBM US', iso2: 'US'},
-    {name: 'RIO AU', iso2: 'AU'},
-    {name: 'BER DE', iso2: 'DE'},
-    {name: 'PHIL NL', iso2: 'NL'},
-    {name: 'CURR IN', iso2: 'IN'}
+    {ric: 'VOD LN', iso2: 'GB'},
+    {ric: 'IBM US', iso2: 'US'},
+    {ric: 'RIO AU', iso2: 'AU'},
+    {ric: 'BER DE', iso2: 'DE'},
+    {ric: 'PHIL NL', iso2: 'NL'},
+    {ric: 'CURR IN', iso2: 'IN'}
 ];
+
+var tradeStatuses = ["unsent", "new", "rejected", "partial", "filled"];
 
 var trades: ITrade[] = [];
 
@@ -39,11 +48,12 @@ for (var t = 1; t < 400; t++)
     var instrument = instruments[Math.floor(Math.random()*instruments.length)];
     trades.push({
         id: t,
-        instrument: instrument.name,
+        instrument: instrument,
         iso2: instrument.iso2,
         side: Math.random() > 0.5 ? Side.Buy : Side.Sell,
+        status: 'new',
         quantity: Math.round(Math.random() * 1000 + 100),
-        filledQuantity: 0
+        filled: 0
     });
 }
 
@@ -51,42 +61,48 @@ for (var t = 1; t < 400; t++)
 // Grid construction
 //
 
-import gridModule = require("lib/biggus-griddus/grid");
+import biggus = require("lib/biggus-griddus/biggus");
+
+var columns: biggus.IColumn<ITrade>[] = [
+    new biggus.TextColumn<ITrade>({title:'ID', path:'id'}),
+    new biggus.ImageColumn<ITrade>({url:"img/flags/{instrument.iso2}.png", lowerCase: true, className: "flag"}),
+    new biggus.TextColumn<ITrade>({title:"Instrument", path:"instrument.ric"}),
+    new biggus.TextColumn<ITrade>({title:"Status", path:"status"}),
+    new biggus.TextColumn<ITrade>({
+        title:"Side",
+        className: "side",
+        value: trade => trade.side === Side.Buy ? "BUY" : "SELL",
+        tdStyle: (td, trade) => td.classList.add(trade.side === Side.Buy ? "buy" : "sell")
+    }),
+    new biggus.TextColumn<ITrade>({title:"Quantity", path:"quantity", className: "numeric"}),
+    new biggus.TextColumn<ITrade>({title:"Filled", path:"filled", className: "numeric"}),
+    new biggus.BarChartColumn<ITrade>({
+        title: "% Filled",
+        className: "fill-percent",
+        ratio: trade => trade.filled/trade.quantity,
+        color: ratio => "hsl(" + (360 * ratio) + ",100%,50%)"
+    }),
+    new biggus.ActionColumn<ITrade>({
+        title:"",
+        text:"cancel",
+        type: biggus.ActionPresentationType.Button,
+        action: trade => alert("Cancelling trade " + trade.id)
+    })
+];
 
 var table = <HTMLTableElement>document.querySelector('table');
 
-var flagColumn = (trade: ITrade) => {
-    var img = document.createElement('img');
-    img.src = 'img/flags/' + trade.iso2.toLowerCase() + '.png';
-    return img;
-};
+var grid = new biggus.Grid<ITrade>(table, {
+    columns: columns,
+    rowClassName: trade => "order-" + trade.status,
+    rowDataId: trade => trade.id.toString()
+});
 
-var filledPercentageColumn = (trade: ITrade) => {
-    var bar = document.createElement('div');
-    bar.className = 'bar';
-    bar.style.width = (100*trade.filledQuantity/trade.quantity) + '%';
-    bar.style.backgroundColor = 'hsl(' + (360 * trade.filledQuantity / trade.quantity) + ',100%,50%)';
-    return bar;
-};
-
-var sideColumn = (trade: ITrade) => trade.side == Side.Buy ? "BUY" : "SELL";
-
-var columnSpecs: gridModule.IColumnSpecification[] = [
-    {title:'ID', field:'id'},
-    {className: 'iso2', field:flagColumn},
-    {title:'Instrument', field:'instrument'},
-    {title:'Side', className: 'side', field:sideColumn},
-    {title:'Quantity', field:'quantity'},
-    {title:'Filled', field:'filledQuantity'},
-    {title:'% Filled', className:'fill-bar', field:filledPercentageColumn}
-];
-
-var grid = new gridModule.Grid<ITrade>(table, {columns: columnSpecs, rowDataId:(trade: ITrade) => trade.id.toString()});
-
-grid.addRows(trades);
+grid.setRows(trades);
 
 setInterval(() => {
     var trade = trades[Math.floor(Math.random()*trades.length)];
-    trade.filledQuantity = Math.floor((trade.filledQuantity + Math.random()*50) % trade.quantity);
-    grid.update(trade.id.toString());
+    trade.filled = Math.floor((trade.filled + Math.random()*50) % trade.quantity);
+    trade.status = tradeStatuses[Math.floor(Math.random() * tradeStatuses.length)];
+    grid.setRow(trade);
 }, 5);
